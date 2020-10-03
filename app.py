72f9ea3ba7ba28b9df4ea278ed9b19b9f3be3517
shaieinav <shai.einav@gmail.com>
2020-10-03 14:54:03 +0300

    Added requirements and files to run the app with uWSGI.

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'shai'
-api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+api = Api(app)
 
 jwt = JWT(app, authenticate, identity) # /auth
 
@@ -30,4 +29,10 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
